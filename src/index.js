5c6022a8c8db1c0d0cb4c0e344e4344a8aab77cb
viachaslau-latushkin <viachaslau.latushkin@gmail.com>
2017-09-20 17:19:28 +0300

    additional_5/solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var brackets = bracketsConfig.map(function(name) {
+		name = name.toString();
+		if(name.match(/[0-9]/g) == null)
+			return "\\" + name.replace(/,/g,"\\");
+		else
+			return name.replace(/,/g,"");
+	});
+	var len1 = str.length;
+	var len2 = 0;
+	while(str.length > 0){
+		if(len1 != len2){
+			len1 = len2;
+			brackets.forEach(function(item) {
+				var re = new RegExp(item,"g");
+				str = str.replace(re,'');
+			});
+			len2 = str.length;
+		}
+		else
+			return false;
+	};
+	return true;
 }
